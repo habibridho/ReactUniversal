13f7e3f44ab49b075a993ed119dc84c05c704271
Habib Ridho <habibridho@live.com>
2017-11-26 11:06:07 +0700

    wrap root components with redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import './web/index.css';
 import App from './web/App';
 import registerServiceWorker from './web/registerServiceWorker';
+import store from "./redux/reducer"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
--- src/index.mobile.js
+++ src/index.mobile.js
@@ -1,4 +1,17 @@
+import React, { Component } from "react"
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux'
 import App from './mobile/App';
+import store from "./redux/reducer"
 
-AppRegistry.registerComponent('ReactUniversal', () => App);
+class Root extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    )
+  }
+}
+
+AppRegistry.registerComponent('ReactUniversal', () => Root);
